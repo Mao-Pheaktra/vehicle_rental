310bfb35b32a78b01dbcf9cb2d1d3961dbe6a86b
unknown <maopheaktra5@gmail.com>
2026-08-12 20:09:46 +0700

    Update dashboard and security configuration

--- src/main/java/org/example/vehicles_rental/configure/SecurityConfig.java
+++ src/main/java/org/example/vehicles_rental/configure/SecurityConfig.java
@@ -26,9 +26,29 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST,"/api/users/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE,"/api/users/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT,"/api/users/**").hasRole("ADMIN")
-                        .requestMatchers("/api/**", "/error/**").permitAll()
 
-                        .anyRequest().authenticated()
+//                        .requestMatchers("/api/brands/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/brands/**").authenticated()
+                        .requestMatchers(HttpMethod.POST,"/api/brands/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE,"/api/brands/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT,"/api/brands/**").hasRole("ADMIN")
+
+
+//                        .requestMatchers("/api/categories/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/api/categories/**").authenticated()
+                                .requestMatchers(HttpMethod.POST,"/api/categories/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE,"/api/categories/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/categories/**").hasRole("ADMIN")
+
+//                                .requestMatchers("/api/vehicle/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/api/vehicle/**").authenticated()
+                                .requestMatchers(HttpMethod.POST,"/api/vehicle/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE,"/api/vehicle/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/vehicle/**").hasRole("ADMIN")
+                        .requestMatchers("/api/vehicle_image/**").hasRole("ADMIN")
+
+                                .requestMatchers("/error/**").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
